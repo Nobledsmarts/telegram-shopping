a18f53ffb65d2a92c7dbf4f0ccd781f759d9fbb1
nobledsmarts <nobledsmarts@gmail.com>
2023-06-03 19:45:26 +0100

    update : product.js

--- model/products.js
+++ model/products.js
@@ -3,5 +3,12 @@ import pizzaImg from '@/public/images/pizza.png'
 export function getData() {
     return [
         { title: "Pizza", price: 17.99, image : pizzaImg },
+        { title: "Burger", price: 15, image : pizzaImg },
+        { title: "Coca", price: 3.5, image : pizzaImg },
+        { title: "Kebab", price: 13.99, image : pizzaImg },
+        { title: "Salad", price: 2.5, image : pizzaImg },
+        { title: "Bottle of water", price: 0.99, image : pizzaImg },
+        { title: "Ice cream", price: 17.99, image : pizzaImg },
+        
     ]
 }
