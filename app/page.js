0a160b6a7ee4522733353afb096759c915b53154
nobledsmarts <nobledsmarts@gmail.com>
2023-06-03 19:41:25 +0100

    add : images to public folder

--- app/page.js
+++ app/page.js
@@ -4,7 +4,10 @@ import { Button } from '@/components/Button'
 export default function Home() {
   return (
     <>
-        <Button title={'test'}/>
+        <Button title={'add'} disable={false} type={'add'}/>
+        <Button title={'remove'} disable={false} type={'remove'}/>
+        <Button title={'chekout'} disable={false} type={'checkout'}/>
+
     </>
   )
 }
---
+++ model/products.js
@@ -0,0 +1,7 @@
+import pizzaImg from '@/public/images/pizza.png'
+
+export function getData() {
+    return [
+        { title: "Pizza", price: 17.99, image : pizzaImg },
+    ]
+}
