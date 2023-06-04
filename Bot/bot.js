f237fc2440ca754483ae950729510e7926efeaa4
nobledsmarts <nobledsmarts@gmail.com>
2023-06-04 04:41:29 +0100

    add : bot.js contents within try catch

--- Bot/bot.js
+++ Bot/bot.js
@@ -4,12 +4,16 @@ const { message } = require('telegraf/filters');
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome 👍', {
-    text : "web app"
-}));
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+try {
+    bot.start((ctx) => ctx.reply('Welcome 👍', {
+        text : "web app"
+    }));
+    // bot.help((ctx) => ctx.reply('Send me a sticker'));
+    // bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+    // bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+    bot.launch();
+} catch (err){
+    console.log('error connecting to bot');
+}
 
 console.log("hello from bot")
